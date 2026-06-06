44c858a2de0145a4ce92d788b6e76d31b6c02e51
fish-hell <156419134+fish-hell@users.noreply.github.com>
2026-06-06 13:22:32 +0900

    Refactor RenderHandler for updated Minecraft API

--- src/main/java/com/wallhack/handler/RenderHandler.java
+++ src/main/java/com/wallhack/handler/RenderHandler.java
@@ -1,16 +1,15 @@
 package com.wallhack.handler;
 
-import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.*;
 import com.wallhack.WallHackMod;
 import com.wallhack.config.EntityConfig;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.*;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.math.Box;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
 import org.joml.Matrix4f;
 
 public class RenderHandler {
@@ -19,23 +18,20 @@ public class RenderHandler {
         WallHackMod mod = WallHackMod.getInstance();
         if (!mod.isWallhackEnabled()) return;
 
-        MinecraftClient client = MinecraftClient.getInstance();
-        if (client.world == null || client.player == null) return;
+        Minecraft client = Minecraft.getInstance();
+        if (client.level == null || client.player == null) return;
 
         EntityConfig config = mod.getEntityConfig();
-        Vec3d cameraPos = context.camera().getPos();
-        MatrixStack poseStack = context.matrixStack();
+        Vec3 cameraPos = context.camera().getPosition();
+        PoseStack poseStack = context.matrixStack();
 
-        RenderSystem.enableBlend();
-        RenderSystem.defaultBlendFunc();
-        RenderSystem.disableDepthTest();
-        RenderSystem.setShader(GameRenderer.getPositionColorProgram());
-
-        for (Entity entity : client.world.getEntities()) {
+        for (Entity entity : client.level.entitiesForRendering()) {
             if (entity == client.player) continue;
-            if (entity instanceof PlayerEntity && !config.isEntityVisible(net.minecraft.util.Identifier.of("minecraft:player"))) continue;
 
-            var entityId = net.minecraft.entity.EntityType.getId(entity.getType());
+            var entityId = net.minecraft.world.entity.EntityType.getKey(entity.getType());
+            if (entityId == null) continue;
+
+            if (entity instanceof Player && !config.isEntityVisible(net.minecraft.resources.ResourceLocation.parse("minecraft:player"))) continue;
             if (!config.isEntityVisible(entityId)) continue;
 
             int color = config.getEntityColor(entityId);
@@ -43,83 +39,69 @@ public class RenderHandler {
             float g = ((color >> 8)  & 0xFF) / 255f;
             float b = (color & 0xFF) / 255f;
 
-            Box box = entity.getBoundingBox().offset(
-                    -cameraPos.x, -cameraPos.y, -cameraPos.z
-            );
-
+            AABB box = entity.getBoundingBox().move(-cameraPos.x, -cameraPos.y, -cameraPos.z);
             renderBox(poseStack, box, r, g, b, 0.2f);
             renderBoxOutline(poseStack, box, r, g, b, 1.0f);
         }
-
-        RenderSystem.enableDepthTest();
-        RenderSystem.disableBlend();
     }
 
-    private static void renderBox(MatrixStack poseStack, Box box, float r, float g, float b, float a) {
-        Matrix4f matrix = poseStack.peek().getPositionMatrix();
-        BufferBuilder buffer = Tessellator.getInstance().begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
+    private static void renderBox(PoseStack poseStack, AABB box, float r, float g, float b, float a) {
+        Matrix4f matrix = poseStack.last().pose();
+        Tesselator tesselator = Tesselator.getInstance();
+        BufferBuilder buffer = tesselator.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_COLOR);
 
-        float x1 = (float) box.minX, y1 = (float) box.minY, z1 = (float) box.minZ;
-        float x2 = (float) box.maxX, y2 = (float) box.maxY, z2 = (float) box.maxZ;
+        float x1 = (float)box.minX, y1 = (float)box.minY, z1 = (float)box.minZ;
+        float x2 = (float)box.maxX, y2 = (float)box.maxY, z2 = (float)box.maxZ;
 
-        // Bottom
-        buffer.vertex(matrix, x1, y1, z1).color(r, g, b, a);
-        buffer.vertex(matrix, x2, y1, z1).color(r, g, b, a);
-        buffer.vertex(matrix, x2, y1, z2).color(r, g, b, a);
-        buffer.vertex(matrix, x1, y1, z2).color(r, g, b, a);
-        // Top
-        buffer.vertex(matrix, x1, y2, z1).color(r, g, b, a);
-        buffer.vertex(matrix, x1, y2, z2).color(r, g, b, a);
-        buffer.vertex(matrix, x2, y2, z2).color(r, g, b, a);
-        buffer.vertex(matrix, x2, y2, z1).color(r, g, b, a);
-        // Front
-        buffer.vertex(matrix, x1, y1, z1).color(r, g, b, a);
-        buffer.vertex(matrix, x1, y2, z1).color(r, g, b, a);
-        buffer.vertex(matrix, x2, y2, z1).color(r, g, b, a);
-        buffer.vertex(matrix, x2, y1, z1).color(r, g, b, a);
-        // Back
-        buffer.vertex(matrix, x1, y1, z2).color(r, g, b, a);
-        buffer.vertex(matrix, x2, y1, z2).color(r, g, b, a);
-        buffer.vertex(matrix, x2, y2, z2).color(r, g, b, a);
-        buffer.vertex(matrix, x1, y2, z2).color(r, g, b, a);
-        // Left
-        buffer.vertex(matrix, x1, y1, z1).color(r, g, b, a);
-        buffer.vertex(matrix, x1, y1, z2).color(r, g, b, a);
-        buffer.vertex(matrix, x1, y2, z2).color(r, g, b, a);
-        buffer.vertex(matrix, x1, y2, z1).color(r, g, b, a);
-        // Right
-        buffer.vertex(matrix, x2, y1, z1).color(r, g, b, a);
-        buffer.vertex(matrix, x2, y2, z1).color(r, g, b, a);
-        buffer.vertex(matrix, x2, y2, z2).color(r, g, b, a);
-        buffer.vertex(matrix, x2, y1, z2).color(r, g, b, a);
+        buffer.addVertex(matrix, x1, y1, z1).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x2, y1, z1).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x2, y1, z2).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x1, y1, z2).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x1, y2, z1).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x1, y2, z2).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x2, y2, z2).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x2, y2, z1).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x1, y1, z1).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x1, y2, z1).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x2, y2, z1).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x2, y1, z1).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x1, y1, z2).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x2, y1, z2).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x2, y2, z2).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x1, y2, z2).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x1, y1, z1).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x1, y1, z2).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x1, y2, z2).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x1, y2, z1).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x2, y1, z1).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x2, y2, z1).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x2, y2, z2).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x2, y1, z2).setColor(r, g, b, a);
 
-        BufferRenderer.drawWithGlobalProgram(buffer.end());
+        BufferUploader.drawWithShader(buffer.buildOrThrow());
     }
 
-    private static void renderBoxOutline(MatrixStack poseStack, Box box, float r, float g, float b, float a) {
-        Matrix4f matrix = poseStack.peek().getPositionMatrix();
-        RenderSystem.lineWidth(1.5f);
-        BufferBuilder buffer = Tessellator.getInstance().begin(VertexFormat.DrawMode.DEBUG_LINES, VertexFormats.POSITION_COLOR);
+    private static void renderBoxOutline(PoseStack poseStack, AABB box, float r, float g, float b, float a) {
+        Matrix4f matrix = poseStack.last().pose();
+        Tesselator tesselator = Tesselator.getInstance();
+        BufferBuilder buffer = tesselator.begin(VertexFormat.Mode.DEBUG_LINES, DefaultVertexFormat.POSITION_COLOR);
 
-        float x1 = (float) box.minX, y1 = (float) box.minY, z1 = (float) box.minZ;
-        float x2 = (float) box.maxX, y2 = (float) box.maxY, z2 = (float) box.maxZ;
+        float x1 = (float)box.minX, y1 = (float)box.minY, z1 = (float)box.minZ;
+        float x2 = (float)box.maxX, y2 = (float)box.maxY, z2 = (float)box.maxZ;
 
-        // Bottom edges
-        buffer.vertex(matrix, x1, y1, z1).color(r, g, b, a); buffer.vertex(matrix, x2, y1, z1).color(r, g, b, a);
-        buffer.vertex(matrix, x2, y1, z1).color(r, g, b, a); buffer.vertex(matrix, x2, y1, z2).color(r, g, b, a);
-        buffer.vertex(matrix, x2, y1, z2).color(r, g, b, a); buffer.vertex(matrix, x1, y1, z2).color(r, g, b, a);
-        buffer.vertex(matrix, x1, y1, z2).color(r, g, b, a); buffer.vertex(matrix, x1, y1, z1).color(r, g, b, a);
-        // Top edges
-        buffer.vertex(matrix, x1, y2, z1).color(r, g, b, a); buffer.vertex(matrix, x2, y2, z1).color(r, g, b, a);
-        buffer.vertex(matrix, x2, y2, z1).color(r, g, b, a); buffer.vertex(matrix, x2, y2, z2).color(r, g, b, a);
-        buffer.vertex(matrix, x2, y2, z2).color(r, g, b, a); buffer.vertex(matrix, x1, y2, z2).color(r, g, b, a);
-        buffer.vertex(matrix, x1, y2, z2).color(r, g, b, a); buffer.vertex(matrix, x1, y2, z1).color(r, g, b, a);
-        // Vertical edges
-        buffer.vertex(matrix, x1, y1, z1).color(r, g, b, a); buffer.vertex(matrix, x1, y2, z1).color(r, g, b, a);
-        buffer.vertex(matrix, x2, y1, z1).color(r, g, b, a); buffer.vertex(matrix, x2, y2, z1).color(r, g, b, a);
-        buffer.vertex(matrix, x2, y1, z2).color(r, g, b, a); buffer.vertex(matrix, x2, y2, z2).color(r, g, b, a);
-        buffer.vertex(matrix, x1, y1, z2).color(r, g, b, a); buffer.vertex(matrix, x1, y2, z2).color(r, g, b, a);
+        buffer.addVertex(matrix, x1, y1, z1).setColor(r, g, b, a); buffer.addVertex(matrix, x2, y1, z1).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x2, y1, z1).setColor(r, g, b, a); buffer.addVertex(matrix, x2, y1, z2).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x2, y1, z2).setColor(r, g, b, a); buffer.addVertex(matrix, x1, y1, z2).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x1, y1, z2).setColor(r, g, b, a); buffer.addVertex(matrix, x1, y1, z1).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x1, y2, z1).setColor(r, g, b, a); buffer.addVertex(matrix, x2, y2, z1).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x2, y2, z1).setColor(r, g, b, a); buffer.addVertex(matrix, x2, y2, z2).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x2, y2, z2).setColor(r, g, b, a); buffer.addVertex(matrix, x1, y2, z2).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x1, y2, z2).setColor(r, g, b, a); buffer.addVertex(matrix, x1, y2, z1).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x1, y1, z1).setColor(r, g, b, a); buffer.addVertex(matrix, x1, y2, z1).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x2, y1, z1).setColor(r, g, b, a); buffer.addVertex(matrix, x2, y2, z1).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x2, y1, z2).setColor(r, g, b, a); buffer.addVertex(matrix, x2, y2, z2).setColor(r, g, b, a);
+        buffer.addVertex(matrix, x1, y1, z2).setColor(r, g, b, a); buffer.addVertex(matrix, x1, y2, z2).setColor(r, g, b, a);
 
-        BufferRenderer.drawWithGlobalProgram(buffer.end());
+        BufferUploader.drawWithShader(buffer.buildOrThrow());
     }
 }
